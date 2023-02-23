1b2dbf3a0083d43bcab24f09fd8737a7549d742a
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2023-02-23 15:01:29 +0100

    update description

--- src/mean_opinion_score_tests/helper.py
+++ src/mean_opinion_score_tests/helper.py
@@ -1,9 +1,5 @@
 from pathlib import Path
 
-import numpy as np
-
-from mean_opinion_score.calculation import get_ci95, get_mos
-
 
 def get_test_resource_path(name: str) -> Path:
   path = Path(f"src/mean_opinion_score_tests/res/{name}")
